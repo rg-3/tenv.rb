58a5d1dde82590e72a9f56c0bec96f0e88e0485d
rg <1xab@protonmail.com>
2020-06-07 14:57:54 +0200

    Add 'random-fav' command

---
+++ lib/tenv/commands/random_fav.rb
@@ -0,0 +1,12 @@
+class Tenv::RandomFav < Tenv::Command
+  match 'random-fav'
+  description 'A random favorite tweet'
+
+  def process
+    tweet = twitter_client.favorites(count: 200, tweet_mode: 'extended').sample
+    out = format "%{tweet}- %{author}",
+          tweet: word_wrap(tweet.full_text),
+          author: bold("@#{tweet.user.screen_name}")
+    puts out
+  end
+end
