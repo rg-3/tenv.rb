ba84f4fcea2af0741101ce78c783dfd66904b28c
rg <1xab@protonmail.com>
2020-06-07 14:58:32 +0200

    Update 'random-tweet' command

--- lib/tenv/commands/random_tweet.rb
+++ lib/tenv/commands/random_tweet.rb
@@ -4,7 +4,7 @@ class Tenv::RandomTweet < Tenv::Command
 
   def process
     tweet = twitter_client.home_timeline(tweet_mode: 'extended').sample
-    out = format "%{tweet}\n- %{author}",
+    out = format "%{tweet}- %{author}",
           tweet: word_wrap(tweet.full_text),
           author: bold("@#{tweet.user.screen_name}")
     puts out
