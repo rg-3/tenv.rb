194114170e52e582127ced588913af9d9c065dfb
rg <1aab@protonmail.com>
2021-01-01 12:03:01 +0100

    update twenv/line.rb

--- lib/twenv/line.rb
+++ lib/twenv/line.rb
@@ -2,9 +2,9 @@
 
 class TWEnv::Line
   DESTRUCTIVE_BACKSPACE = "\b \b"
-  RED_ERROR_TEXT = Paint["ERR", "#CC0000", :bright]
-  GREEN_OK_TEXT  = Paint["OK", "#00FF00", :bright]
-  ORANGE_WARNING_TEXT = Paint["WARN", "#FFA500", :bright]
+  RED_ERROR = Paint["ERR", "#CC0000", :bright]
+  GREEN_OK  = Paint["OK", "#00FF00", :bright]
+  ORANGE_WARNING = Paint["WARN", "#FFA500", :bright]
 
   def initialize(io)
     @io = io
@@ -23,15 +23,15 @@ class TWEnv::Line
   end
 
   def error(message)
-    print "#{RED_ERROR_TEXT} #{message[0].upcase}#{message[1..-1]}"
+    print "#{RED_ERROR} #{message[0].upcase}#{message[1..-1]}"
   end
 
   def ok(message)
-    print "#{GREEN_OK_TEXT} #{message[0].upcase}#{message[1..-1]}"
+    print "#{GREEN_OK} #{message[0].upcase}#{message[1..-1]}"
   end
 
   def warn(message)
-    print "#{ORANGE_WARNING_TEXT} #{message[0].upcase}#{message[1..-1]}"
+    print "#{ORANGE_WARNING} #{message[0].upcase}#{message[1..-1]}"
   end
 
   def end
@@ -40,7 +40,7 @@ class TWEnv::Line
   end
 
   def rewind
-    @io.print Array.new(@size) { DESTRUCTIVE_BACKSPACE }.join
+    @io.print DESTRUCTIVE_BACKSPACE * @size
     self
   end
 end
